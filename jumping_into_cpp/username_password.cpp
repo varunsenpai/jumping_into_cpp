58e856322bbeac603b33ad7cf2ec9b94a18d7e38
varun puppala <varun_puppala@yahoo.co.in>
2021-12-14 00:15:32 +0100

    added the function capabality to password username auth prog

--- jumping_into_cpp/username_password.cpp
+++ jumping_into_cpp/username_password.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+int check_creds(vector<pair<string, string>>&, string, string);
+
 int main()
 {
 	string username;
@@ -23,14 +25,26 @@ int main()
 
 	cout << "Enter the password" << endl;
 	getline(cin, passwd, '\n');
+
+	//check the credentials
+	auto ret_val = check_creds(s1, username, passwd);
+
+	cout << "Done with return value " << ret_val << endl;
 	
+	return 0;
+}
+
+int check_creds(vector<pair<string, string>>& s1, string username, string passwd)
+{
+	int flag = 0;
+
 	for (auto i = s1.begin(); i != s1.end(); ++i)
 	{
-		if ((username == i -> first) && (passwd == i -> second))
+		if ((username == i->first) && (passwd == i->second))
 		{
 			cout << "Authentication Successfull" << endl;
 			flag = 1;
-			break;
+			return 1;
 		}
 
 	}
@@ -38,7 +52,7 @@ int main()
 	if (!flag)
 	{
 		cout << "Authentication Failure" << endl;
+		return 0;
 	}
 
-	return 0;
 }
