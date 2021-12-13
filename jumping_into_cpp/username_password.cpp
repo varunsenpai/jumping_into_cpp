895a994f8670bf76246e3da9f70b3fe39bc91bc4
varun puppala <varun_puppala@yahoo.co.in>
2021-12-13 10:46:19 +0100

    updated this branch with more records of usernames and passwords

--- jumping_into_cpp/username_password.cpp
+++ jumping_into_cpp/username_password.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <utility>
 
 using namespace std;
 
@@ -9,28 +10,34 @@ int main()
 	string username;
 	string passwd;
 
-	string stored_username = "varun";
-	string stored_passwd = "password";
+	int flag = 0;
 
-	vector<string> my_record;
+	vector<pair<string, string>> s1;
 
-	my_record.push_back("puppala");
-	
+	s1.push_back(make_pair("puppala", "password1"));
+	s1.push_back(make_pair("puppala1", "password2"));
+	s1.push_back(make_pair("varun", "password"));
 
 	cout << "Enter the username" << endl;
 	getline(cin, username, '\n');
 
 	cout << "Enter the password" << endl;
 	getline(cin, passwd, '\n');
-
-	if ((username == stored_username) && (passwd == stored_passwd))
+	
+	for (auto i = s1.begin(); i != s1.end(); ++i)
 	{
-		cout << "Authentication Successfull" << endl;
+		if ((username == i -> first) && (passwd == i -> second))
+		{
+			cout << "Authentication Successfull" << endl;
+			flag = 1;
+			break;
+		}
+
 	}
 
-	else
+	if (!flag)
 	{
-		cout << "Authentication Unsuccessfull" << endl;
+		cout << "Authentication Failure" << endl;
 	}
 
 	return 0;
